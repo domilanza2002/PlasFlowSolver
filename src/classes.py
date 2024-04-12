4b5dd0af4c5469dc63474e334f7fd0e936d7e4ab
Domenico Lanza <domilanza2002@gmail.com>
2024-04-12 15:30:50 -0500

    minor

--- src/classes.py
+++ src/classes.py
@@ -1,40 +1,114 @@
 #.................................................
-#   CLASSES.PY, v3.0.0, February 2024, Domenico Lanza.
+#   CLASSES.PY, v1.0.0, April 2024, Domenico Lanza.
 #.................................................
-#   This file contains the classes of the program.
+#   This module contains all the classes used in the program.
 #   There are currently 5 classes:
+#   -dataframe_class: class to store all the input variables
 #   -inputs_class: class to store all the inputs of the program for the current iteration
 #   -settings_class: class to store all the settings of the program
 #   -probes_class: class to store all the probes properties
 #   -initials_class: class to store all the initial conditions
-#   -dataframe_xlsx_class: class to store the dataframe variables from the xlsx file
 #.................................................
-class inputs_class: #class to store all the inputs of the program for the current iteration
-    """This class contains the inputs of the program for the current iteration
+#.................................................
+class dataframe_class:
+    """This class contains all the variables read from
+    the input files, regardless of the type of file.
     """
-    #.................................................
-    #   This class contains the inputs of the program for the current iteration
-    #.................................................
-    def __init__(self): #basic constructor
-        comment = None #Comment, string
-        P = None #Pressure, float
-        Pdyn = None #Dynamic pressure, float
-        Pstag = None #Stagnation pressure, float
-        q = None #Heat flux, float
-        plasma_gas = None #Plasma gas, string
-        P_CF = None #Static pressure conversion factor, float
-        PD_CF = None #Dynamic pressure conversion factor, float
-        PS_CF = None #Stagnation pressure conversion factor, float
-        Q_CF = None #Heat flux conversion factor, float
-    
-class settings_class: #class to store all the settings of the program
+    def __init__(self):  # Basic constructor
+        # To be computed:
+        n = None  # Number of cases (integer)
+        # To be read from the file:
+        # Inputs:
+        comment = None  # Comment (string)
+        P = None  # Pressure (float)
+        P_dyn = None  # Dynamic pressure (float)
+        P_stag = None  # Stagnation pressure (float)
+        q_target = None  # Target heat flux (float)
+        plasma_gas = None  # Plasma gas (string)
+        # Conversion factors:
+        P_CF = None  # Static pressure conversion factor (float)
+        P_dyn_CF = None  # Dynamic pressure conversion factor (float)
+        P_stag_CF = None  # Stagnation pressure conversion factor (float)
+        q_CF = None  # Heat flux conversion factor (float)
+        # Initial conditions:
+        T_0 = None  # Initial static temperature (float)
+        T_t_0 = None  # Initial total temperature (float)
+        u_0 = None  # Initial flow velocity (float)
+        P_t_0 = None  # Initial total pressure (float)
+        # Probe settings:
+        T_w = None  # Probe wall temperature (float)
+        R_p = None  # Pitot external radius (float)
+        R_m = None  # External radius of the heat flux probe (float)
+        R_j = None  # Plasma jet radius (float)
+        stag_type = None  # Stagnation type (string)
+        hf_law = None  # Heat flux law (string)
+        barker_type = None  # Barker's correction type (string)
+        # Program settings:
+        N_p = None  # Number of point for the discretization of normal coordinate of the boundary layer (integer)
+        max_hf_iter = None  # Maximum number of iterations for the heat flux computation (integer)
+        hf_conv = None  # Convergence criteria for the heat flux computation (float)
+        use_prev_ite = None  # Flag to indicate if the previous iteration for the heat flux computation should be
+        # used as initial guess for the new iteration (string)
+        eta_max = None  # Upper integration boundary for the normal coordinate of the boundary layer (float)
+        log_warning_hf = None  # Flag to indicate if a warning should be logged when the heat flux computation does not converge (string)
+        newton_conv = None  # Convergence criteria for the Newton-Raphson method (float)
+        max_newton_iter = None  # Maximum number of iterations for Newton-Raphson method (integer)
+        jac_diff = None  # Finite difference epsilon for the Jacobian matrix (float)
+        min_T_relax = None  # Minimum ammissible value for the temperature, used for relaxation (float)
+        max_T_relax = None  # Maximum ammissible value for the temperature, used for relaxation (float)
+
+#..................................................
+class inputs_class: 
+    """This class contains the thermodynamic inputs of 
+    the program for the current case.
+    """
+    def __init__(self):  # Basic constructor
+        comment = None  # Comment (string)
+        P = None  # Static pressure (float)
+        P_dyn = None  # Dynamic pressure (float)
+        P_stag = None  # Stagnation pressure (float)
+        q_target = None  # Target heat flux (float)
+        mixture_name = None  # Mixture name (string)
+        P_CF = None  # Static pressure conversion factor (float)
+        P_dyn_CF = None  # Dynamic pressure conversion factor (float)
+        P_stag_CF = None  # Stagnation pressure conversion factor (float)
+        q_CF = None  # Heat flux conversion factor (float)
+        
+#.................................................
+class initials_class:
+    """This class contains the initial conditions 
+    of the program for the current case.
+    """
+    def __init__(self):  # Basic constructor
+        T_0 = None  # Initial static temperature (float)
+        T_t_0 = None  # Initial total temperature (float)
+        u_0 = None  # Initial flow velocity (float)
+        P_t_0 = None  # Initial total pressure (float)
+
+#.................................................
+class probes_class:
+    """This class contains the probe settings
+    for the current case.
+    """
+    def __init__(self):  # Basic constructor
+        T_w = None #Wall temperature, float
+        R_p = None #Pitot external radius, float
+        R_m = None #Flux probe external radius, float
+        R_j = None #Plasma jet radius, float
+        stag_type = None # Stagnation type, string->integer
+        hf_law = None #Heat flux law, string->integer
+        barker_type = None #Barker correct, string->integer
+        stagvar = None #Stagnation variable, float
+
+#..................................................
+class settings_class:
     """This class contains the settings of the program
     """
     #.................................................
     #   This class contains the settings of the program
     #.................................................
     def __init__(self): #basic constructor
-        p = None #Number of point for the boundary layer eta discretization, integer
+        N_p = None #Number of point for the boundary layer eta discretization, integer
         max_hf_iter = None #Maximum number of iterations for the heat flux, integer
         hf_conv = None #Convergence criteria for the heat flux, float
         use_prev_ite = None #Use previous iteration for the heat transfer, string
@@ -45,79 +119,7 @@ class settings_class: #class to store all the settings of the program
         jac_diff = None #Main newton jacobian finite difference epsilon, float
         min_T_relax = None #Minimum value for the temperature used for relaxation, float
         max_T_relax = None #Maximum value for the temperature used for relaxation, float
-#.................................................
-class probes_class: #class to store all the probes properties
-    """This class contains the properties of the probe
-    """
-    #.................................................
-    #   This class contains the properties of the probe
-    #.................................................
-    def __init__(self): #basic constructor
-        Tw = None #Wall temperature, float
-        Rp = None #Pitot external radius, float
-        Rm = None #Flux probe external radius, float
-        Rj = None #Plasma jet radius, float
-        stagtype = None #Stagnation type, string->integer
-        hflaw = None #Heat flux law, string->integer
-        barker = None #Barker correct, string->integer
-        stagvar = None #Stagnation variable, float
-#.................................................
-class initials_class: #class to store all the initial conditions
-    """This class contains the initial conditions of the program
-    """
-    #.................................................
-    #   This class contains the initial conditions of the program
-    #.................................................
-    def __init__(self): #basic constructor
-        T_0=None #Initial static temperature, float
-        Tt_0=None #Initial total temperature, float
-        u_0=None #Initial velocity, float
-        Pt_0=None #Initial total pressure, float
-#.................................................
-class dataframe_xlsx_class: #class to store the dataframe variables from the xlsx file
-    #   This class contains the dataframe variables from the xlsx file, read using pandas library.
-    """This class contains the dataframe variables from the xlsx file, read using pandas library.
-    """
-    def __init__(self): #basic constructor
-        #To be calculated
-        n = None #Number of the test, integer
-        #To be read from the xlsx file
-        #   -Inputs
-        comment = None #Comment, string
-        P = None #Pressure, float
-        Pdyn = None #Dynamic pressure, float
-        Pstag = None #Stagnation pressure, float
-        q = None #Heat flux, float
-        plasma_gas = None #Plasma gas, string
-        #   -Conversion factors
-        P_CF=None #Static pressure conversion factor, float
-        PD_CF=None #Dynamic pressure conversion factor, float
-        Q_CF=None #Heat flux conversion factor, float
-        #   -Initial conditions
-        T_0=None #Initial static temperature, float
-        Tt_0=None #Initial total temperature, float
-        u_0=None #Initial velocity, float
-        Pt_0=None #Initial total pressure, float
-        #   -Probes settings
-        Tw = None #Wall temperature, float
-        Rp = None #Pitot external radius, float
-        Rm = None #Flux probe external radius, float
-        Rj = None #Plasma jet radius, float
-        stagtype = None #Stagnation type, string->integer
-        hflaw = None #Heat flux law, string->integer
-        barker = None #Barker correct, string->integer
-        #   -Settings
-        p = None #Number of point for the boundary layer eta discretization, integer
-        max_hf_iter = None #Maximum number of iterations for the heat flux, integer
-        hf_conv = None #Convergence criteria for the heat flux, float
-        use_prev_ite = None #Use previous iteration for the heat transfer, string
-        newton_conv = None #Convergence criteria for the newton solver, float
-        max_newton_iter = None #Maximum number of iterations for the newton solver, integer
-        jac_diff = None #Main newton jacobian finite difference epsilon, float
-        max_T_relax = None #Maximum value for the temperature used for relaxation, float
-        min_T_relax = None #Minimum value for the temperature used for relaxation, float
-        log_warning_hf = None #Log warning for when the heat flux does not converge, string
-        eta_max = None #Maximum value for the boundary layer eta, float
+
 #.................................................
 #   Possible improvements:
 #   -Add getters and setters and make all the variable private
