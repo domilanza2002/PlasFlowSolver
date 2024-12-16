63871898a84979ea1959ee9e88f4745738e8b48a
Domenico Lanza <domilanza2002@gmail.com>
2024-12-16 13:57:25 +0100

    barker

--- barker_effect.py
+++ barker_effect.py
@@ -1,12 +1,13 @@
 #.................................................
-#   BARKER_EFFECT.PY, v1.0.0, April 2024, Domenico Lanza.
+#   BARKER_EFFECT.PY, v2.0.0, December 2024, Domenico Lanza.
 #.................................................
-#   This module is needed to compute the Barker's effect.
+#   This module is needed to compute the Barker effect.
 #.................................................
-import mutationpp as mpp  # Thermodynamic library
 import math  # Math library
+
 def barker_effect(probes, mixture_object, P_t, P, T, u):
-    """This function returns the barker pressure given the total pressure, the pressure, the temperature and the velocity
+    """This function returns the barker pressure given the total pressure, 
+    the static pressure, the temperature and the velocity.
 
     Args:
         probes (probes_class): probe properties
@@ -19,18 +20,16 @@ def barker_effect(probes, mixture_object, P_t, P, T, u):
         P_b (float): barker pressure
         Re (float): reynolds number
     """
-    # Variables:
-    barker_type = probes.barker_type  # Barker's correction type
+    # Extract:
+    barker_type = probes.barker_type  # Barker correction type
     R_p = probes.R_p  # Pitot external radius
-    rho = None  # Flow density
-    mu = None  # Flow viscosity
-    C_p = None  # Barker's correction C_p
-    P_b = None  # Barker's stagnation pressure read
-    Re = None  # Reynolds number on the pitot probe
-    mixture_object.equilibrate(T, P)  # I equilibrate the mixture
-    rho = mixture_object.density()  # I get the density
-    mu = mixture_object.viscosity()  # I get the viscosity
-    Re = rho*u*(2*R_p)/mu  # Barker's Reynolds number
+    # Compute:
+    mixture_object.equilibrate(T, P)
+    rho = mixture_object.density() 
+    mu = mixture_object.viscosity()
+    # Compute Reynolds number
+    Re = rho*u*(2*R_p)/mu 
+    # Match the Barker type
     match (barker_type):
         case 0:  # No barker effect
             C_p = 0
@@ -39,16 +38,14 @@ def barker_effect(probes, mixture_object, P_t, P, T, u):
         case 2:  # Carleton's correction
             C_p = 1 + 8/(Re+0.5576*math.sqrt(Re))
         case _:
-            print("Error: Barker's correction not yet implemented. But you should not be there... check retrieve_helper.py")
+            print("Error: Barker's correction not yet implemented. You should not see this message. Check retrieve_helper.py")
             exit()
-    # Barker's pressure (stagnation pressure read instead of the total pressure)
+    # Barker pressure (stagnation pressure read instead of the total pressure)
     P_b = P_t + 0.5*rho*pow(u, 2)*C_p
     return P_b, Re
 #.................................................
 #   Possible improvements:
 #   - Add other barker computations
-#.................................................
-# EXECUTION TIME: TBD
 #.................................................
 #   KNOW PROBLEMS:
 #   None.
